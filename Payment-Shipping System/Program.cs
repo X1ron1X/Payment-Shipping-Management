ba983f219fa7637dce7f9f171fee4b61a847a19e
jobelaraw <jobelaraw7@gmail.com>
2026-02-21 12:20:10 +0800

    Initial commit

---
+++ Payment-Shipping System/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Payment_Shipping_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ADDRESS");
+
+            Console.WriteLine("Full Name: ");
+            string fname = Console.ReadLine();
+            Console.WriteLine("Phone Number: ");
+            string pnum = Console.ReadLine();
+            Console.WriteLine("Region: ");
+            string reg = Console.ReadLine();
+            Console.WriteLine("Province: ");
+            string prov = Console.ReadLine();
+            Console.WriteLine("City: ");
+            string city = Console.ReadLine();
+            Console.WriteLine("Barangay: ");
+            string brgy = Console.ReadLine();
+            Console.WriteLine("Postal Code: ");
+            string pcode = Console.ReadLine();
+            Console.WriteLine("Street Name, Building, House No.: ");
+            string sbh = Console.ReadLine();
+        }
+    }
+}
