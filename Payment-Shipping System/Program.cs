3854860f865f48710ca75a2a60c6f3a7a53fb315
X1ron1X <159204510+X1ron1X@users.noreply.github.com>
2026-02-28 10:46:45 +0800

    Payment methods

--- Payment-Shipping System/Program.cs
+++ Payment-Shipping System/Program.cs
@@ -7,35 +7,40 @@ namespace Payment_Shipping_System
         static void Main(string[] args)
         {
 
+            bool chose = addchoice();
+
+            while (chose)
+            {
+                Console.WriteLine("[1] Payment Methods\n[2] Address");
+
+                Console.Write("\nEnter(1/2): ");
+                string opt1 = Console.ReadLine();
+
+                if (opt1 == "1")
+                {
+                    addpayment();
+                    addchoice();
+                }
+                else if (opt1 == "2")
+                {
+                    addaddress();
+                    addchoice();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+
+            }
+        }
+
+        static bool addchoice()
+        {
+
             Console.Write("Do You Want to Add a ADDRESS(y/n): ");
             bool chose = false;
             string choice = Console.ReadLine();
 
-            
-
-            Console.WriteLine("\nADDRESS");
-
-            Console.Write("\nFull Name: ");
-            string fname = Console.ReadLine();
-            Console.Write("\nPhone Number: ");
-            string pnum = Console.ReadLine();
-            Console.Write("\nRegion: ");
-            string reg = Console.ReadLine();
-            Console.Write("\nProvince: ");
-            string prov = Console.ReadLine();
-            Console.Write("\nCity: ");
-            string city = Console.ReadLine();
-            Console.Write("\nBarangay: ");
-            string brgy = Console.ReadLine();
-            Console.Write("\nPostal Code: ");
-            string pcode = Console.ReadLine();
-            Console.Write("\nStreet Name, Building, House No.: ");
-            string sbh = Console.ReadLine();
-        }
-
-        static bool addchoice(bool chose, string choice){
-
-            
 
             switch (choice)
             {
@@ -55,6 +60,41 @@ namespace Payment_Shipping_System
             }
 
             return chose;
+
+        }
+
+        static void addpayment()
+        {
+            Console.WriteLine("\nPAYMENT METHODS\n");
+            Console.Write("\nCard Number: ");
+            string cnum = Console.ReadLine();
+            Console.Write("\nCard Holder Name: ");
+            string chname = Console.ReadLine();
+            Console.Write("\nExpiration Date(MM/YY): ");
+            string edate = Console.ReadLine();
+            Console.Write("\nCVV: ");
+            string cvv = Console.ReadLine();
+        }
+
+        static void addaddress()
+        {
+            Console.WriteLine("\nADDRESS\n");
+            Console.Write("\nFull Name: ");
+            string fname = Console.ReadLine();
+            Console.Write("\nPhone Number: ");
+            string pnum = Console.ReadLine();
+            Console.Write("\nRegion: ");
+            string reg = Console.ReadLine();
+            Console.Write("\nProvince: ");
+            string prov = Console.ReadLine();
+            Console.Write("\nCity: ");
+            string city = Console.ReadLine();
+            Console.Write("\nBarangay: ");
+            string brgy = Console.ReadLine();
+            Console.Write("\nPostal Code: ");
+            string pcode = Console.ReadLine();
+            Console.Write("\nStreet Name, Building, House No.: ");
+            string sbh = Console.ReadLine();
 
         }
     }
