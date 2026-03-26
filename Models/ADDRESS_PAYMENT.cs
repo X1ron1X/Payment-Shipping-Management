ffc90a3c5ed6652d6428a190d086eac4b3d8eacd
X1ron1X <159204510+X1ron1X@users.noreply.github.com>
2026-03-26 12:12:06 +0800

    id

--- Models/ADDRESS_PAYMENT.cs
+++ Models/ADDRESS_PAYMENT.cs
@@ -2,6 +2,7 @@
 {
     public class ADD
     {
+        public Guid AID { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
         public string PNumber { get; set; }
@@ -11,6 +12,7 @@
 
     public class Card
     {
+        public Guid CID { get; set; }
         public string Name { get; set; }
         public string CNumber { get; set; }
         public string EDate { get; set; }
@@ -20,6 +22,7 @@
 
     public class Bank
     {
+        public Guid BID { get; set; }
         public string Holder { get; set; }
         public string BNumber { get; set; }
         public string BName { get; set; }
@@ -28,6 +31,7 @@
 
     public class Gcash
     {
+        public Guid GID { get; set; }
         public string Name { get; set; }
         public string GNumber { get; set; }
 
